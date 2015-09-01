e340bd765a9eb4584e8fda4a4e28ca4da651b556
Christian Petrov <cpetrov@eclipsesource.com>
2015-09-01 11:52:30 +0200

    Improve DatePicker example
    The example now also demonstrates a programmatic change of the date
    property.

--- examples/datepicker/datepicker.js
+++ examples/datepicker/datepicker.js
@@ -2,12 +2,19 @@ require("tabris-datepicker");
 
 var page = tabris.create("Page", {topLevel: true, title: "DatePicker"});
 
-tabris.create("DatePicker", {
-  layoutData: {left: 0, top: 0}
-}).appendTo(page).on("select", function() {
+var datePicker = tabris.create("DatePicker", {
+  layoutData: {centerX: 0, centerY: 0}
+}).on("select", function() {
   console.log("date selected: " + this.get("date"));
 }).on("change:date", function() {
   console.log("date property changed: " + this.get("date"));
-});
+}).appendTo(page);
+
+tabris.create("Button", {
+  text: "Set date to the beginning of the third millennium",
+  layoutData: {centerX: 0, top: [datePicker, 10]}
+}).on("select", function() {
+  datePicker.set("date", new Date(2001, 0, 1));
+}).appendTo(page);
 
 page.open();
